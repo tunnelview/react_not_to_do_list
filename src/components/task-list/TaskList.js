6f03b800dd80f172babba91be4422e68095056e4
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-04-02 12:56:41 +1100

    Add Bad List to TaskList

--- src/components/task-list/TaskList.js
+++ src/components/task-list/TaskList.js
@@ -15,7 +15,36 @@ const TaskList = () => {
                 <td>
                   <FormCheck type="checkbox" label="Check me out" />
                 </td>
-                <td> task Name</td>
+                <td> Task Name</td>
+                <td>10 hr</td>
+                <td className="text-end">
+                  <Button variant="danger">
+                    <i className="fa-solid fa-trash-can"></i>
+                  </Button>{" "}
+                  <Button variant="primary">
+                    <i className="fa-solid fa-arrow-right-long"></i>
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+
+        <Col md="6">Right</Col>
+      </Row>
+
+      <Row>
+        <Col md="6">
+          <h2 className="text-center"> Bad List</h2>
+          <hr />
+
+          <Table striped hover>
+            <tbody>
+              <tr>
+                <td>
+                  <FormCheck type="checkbox" label="Check me out" />
+                </td>
+                <td> Task Name</td>
                 <td>10 hr</td>
                 <td className="text-end">
                   <Button variant="danger">
