d584e81468a70e6da12b6466f103ab03b25f44ad
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-04-03 12:26:09 +1000

    Add BadList.js to task-list folder

---
+++ src/components/task-list/BadList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button, Col, Form, FormCheck, Row, Table } from "react-bootstrap";
+
+const TaskList = () => {
+  return (
+    <div>
+      <h2 className="text-center"> Task List</h2>
+      <hr />
+
+      <Table striped hover>
+        <tbody>
+          <tr>
+            <td>
+              <FormCheck type="checkbox" label="Check me out" />
+            </td>
+            <td> Task Name</td>
+            <td>10 hr</td>
+            <td className="text-end">
+              <Button variant="danger">
+                <i className="fa-solid fa-trash-can"></i>
+              </Button>{" "}
+              <Button variant="primary">
+                <i className="fa-solid fa-arrow-right-long"></i>
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default TaskList;
